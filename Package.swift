bb8a98cefd09a96a0b299274c11a2aab4d951782
alandmorford <alan@violet.io>
2023-07-10 12:42:41 -0700

    Update the Package to newer tool chain

--- Package.swift
+++ Package.swift
@@ -1,21 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VioletPublicClientAPI",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_11),
-        .tvOS(.v9),
-        .watchOS(.v3),
+        .iOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VioletPublicClientAPI",
-            targets: ["VioletPublicClientAPI"]
-        ),
+            targets: ["VioletPublicClientAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -23,11 +20,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "VioletPublicClientAPI",
-            dependencies: ["AnyCodable", ],
-            path: "Sources/VioletPublicClientAPI"
-        ),
+            dependencies: ["AnyCodable", ]),
+        .testTarget(
+            name: "VioletPublicClientAPITests",
+            dependencies: ["VioletPublicClientAPI"]),
     ]
 )
---
+++ Tests/VioletPublicClientAPITests/VioletPublicClientAPITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VioletPublicClientAPI
+
+final class VioletPublicClientAPITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VioletPublicClientAPI().text, "Hello, World!")
+    }
+}
